56097b259202a71b27f130cb0e1547798e57e473
bala-amypo <bala@amypo.in>
2025-12-16 10:33:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ServiceCounter.java
@@ -0,0 +1,8 @@
+package com.example.demo.entity;
+
+public class ServiceCounter{
+    private Long id;
+    private String counterName;
+    private String department;
+    private Boolean isActive;
+}
