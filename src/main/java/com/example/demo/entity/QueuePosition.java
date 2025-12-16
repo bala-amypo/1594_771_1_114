7ea0174c5dcfa72e23e8db982fdd00f887de3b9a
bala-amypo <bala@amypo.in>
2025-12-16 10:38:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/QueuePosition.java
@@ -0,0 +1,8 @@
+pakage com.example.demo.entity;
+
+public class QueuePosition{
+    private Long id;
+    private Token token;
+    private Integer position;
+    private LocalDateTime 
+}
--- src/main/java/com/example/demo/entity/Token.java
+++ src/main/java/com/example/demo/entity/Token.java
@@ -2,5 +2,9 @@ package com.example.demo.entity;
 
 public class Token{
     private Long id;
-    
+    private String tokenNumber;
+    private ServiceCounter ServiceCounter;
+    private String status;
+    private LocalDateTime issuedAt;
+    private LocalDateTime completedAt;
 }
