3530131b5fcf186216bd96f41f34651f7aea479b
bala-amypo <bala@amypo.in>
2025-12-18 09:53:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -13,7 +13,7 @@ public class User {
     private String name;
     private String email;
     private String password;
-    private String role; // ADMIN / STAFF
+    private String role;
 
     public User() {}
 
@@ -24,5 +24,15 @@ public class User {
         this.role = role;
     }
 
-    // getters & setters
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setRole(String role) { this.role = role; }
 }
