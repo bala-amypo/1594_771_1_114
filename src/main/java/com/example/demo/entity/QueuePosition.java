f3e9841fed285b76471b046f5f92912709cd1df8
bala-amypo <bala@amypo.in>
2025-12-18 08:29:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/QueuePosition.java
+++ src/main/java/com/example/demo/entity/QueuePosition.java
@@ -1,9 +1,25 @@
-pakage com.example.demo.entity;
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class QueuePosition{
+@Entity
+@Getter
+@Setter
+public class QueuePosition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "token_id")
     private Token token;
+
     private Integer position;
+
     private LocalDateTime updatedAt;
 }
