91a3be70ed4e9c3c4e880da0ca6b9a32a7e3f725
bala-amypo <bala@amypo.in>
2025-12-19 04:55:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+          .csrf(csrf -> csrf.disable())
+          .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
