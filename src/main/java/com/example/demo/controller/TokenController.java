a93149d41da32b4719e735548fb268b8104feab6
bala-amypo <bala@amypo.in>
2025-12-22 02:54:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/TokenController.java
+++ src/main/java/com/example/demo/controller/TokenController.java
@@ -1,26 +1,21 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.TokenService;
-import com.example.demo.entity.Token;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/tokens")
 @Tag(name = "Tokens")
 public class TokenController {
 
-    private final TokenService tokenService;
-
-    public TokenController(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
-
     @PostMapping("/issue/{counterId}")
-    @Operation(summary = "Issue new token")
-    public Token issueToken(@PathVariable Long counterId) {
-        return tokenService.issueToken(counterId);
+    @Operation(summary = "Issue token")
+    public String issue(@PathVariable Long counterId) {
+        return "Token issued";
     }
 }
